b3cfa03f2150f67622a273dfdf5a3601cd6a92cc
Rolandas Šimkus <rolandas@simkus.io>
2026-01-19 06:33:23 +0200

    working

--- clang/1.9.c
+++ clang/1.9.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#define MAXLINE 1000 // maximum input line length
+
+int getline(char line[], int maxline);
+void copy(char to[], char from[]);
 
 int main()
 {
     return 0;
+}
+
+
+// getline: read a line into s, return length
+int getline(char s[], int lim)
+{
+    int c, i;
+
+
+    return i;
 }
