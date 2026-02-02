bc969e16f0cee025b5e9a5afec721614936f9f9c
Rolandas Šimkus <rolandas@simkus.io>
2026-02-02 10:17:29 +0200

    fix getlinec func to properly count line len

--- clang/1.9.c
+++ clang/1.9.c
@@ -34,16 +34,16 @@ int main()
 int getlinec(char s[], int lim){
     int c, i;
 
-    for (i=0; i < lim-1 && (c=getchar()) != EOF && c != '\n'; i++){
+    for (i=0; i < lim-1 && (c=getchar()) != EOF && c != '\n'; ++i){
         s[i] = c;
-
-        if (c == '\n'){
-            s[i] = c;
-            ++i;
-        }
-
-        s[i] = '\0';
     }
+
+    if (c == '\n'){
+        s[i] = c;
+        ++i;
+    }
+
+    s[i] = '\0';
 
     return i;
 }
